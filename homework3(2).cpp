f2a71fa4544e15a0457f1fd1e586f0bc4af594aa
KamTagana <taganidze@gmail.com>
2022-01-17 16:10:57 +1200

    Create homework3(2).cpp
    Задание 2

---
+++ homework3(2).cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int a = 10, b;
+	b = (a <= 21) ? 21 - a : (21 - a) * 2;
+	std::cout << b << std::endl;
+
+	int c = 30, d;
+	d = (c <= 21) ? 21 - c : (21 - c) * 2;
+	std::cout << d << std::endl;
+}
