d79b388e46fa3ca04016a4ca2a4b0853ba5d61e2
KamTagana <taganidze@gmail.com>
2022-01-25 19:47:59 +1200

    Урок 5. Функции.

---
+++ task1.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+
+void __fastcall MyFunc(const double arr[], const size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+
+}
+
+int main()
+{
+	const size_t size = 5;
+	double arr[] = { 11.1, 12.2, 13.3, 14.4, 15.5 };
+
+	MyFunc(arr, size);
+
+	cout << endl;
+}
---
+++ task3.cpp
@@ -0,0 +1,41 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+void __fastcall MyFunc(const int arr[], const size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+}
+
+bool __fastcall Array(int* arr, const size_t size)
+{
+    const int step = 3;
+    
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = i * step + 1;
+    }
+    return true;
+}
+
+int main()
+{
+    const size_t size = 8;
+    int arr[size];
+
+    if (Array(arr, size))
+    {
+        
+        MyFunc(arr, size);
+        cout << endl;
+    }
+}
+
