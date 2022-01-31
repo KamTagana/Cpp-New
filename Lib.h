6ba4417aaefac30299021013df4405ace56ee192
KamTagana <taganidze@gmail.com>
2022-01-31 23:59:48 +1200

    Урок 7. Препроцессинг

---
+++ Lib.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Lib 
+{
+	void PrintArray(const float*, const size_t);
+	void PrintArray(const int*, const size_t);
+	float* createAndIniArray(const size_t);
+	void PrintPosAndNeg(const float*, const size_t);
+	void SortArray(int*, const size_t);
+}
---
+++ hw1.cpp
@@ -0,0 +1,72 @@
+﻿
+#include <iostream>
+#include <fstream>
+
+#include "Lib.h"
+
+
+using namespace std;
+
+//=================
+
+inline void task1()
+{
+    const short size = 10;
+    const float* arr = Lib::createAndIniArray(size);
+
+    if (arr != nullptr)
+    {
+        Lib::PrintArray(arr, size);
+        Lib::PrintPosAndNeg(arr, size);
+
+        delete[] arr;
+    }
+}
+
+//=================
+
+#define IN_RANGE(x,y) ((x >= 0 && x < y) ? "true" : "false")
+
+inline void task2()
+{
+    int num;
+    cin >> num;
+    cout << IN_RANGE(num, 10) << endl;
+}
+
+//=================
+
+#define ARR_SIZE 10
+
+inline void task3()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int* arr = new (nothrow) int[ARR_SIZE];
+
+    if (arr != nullptr)
+    {
+        cout << "Введите " << ARR_SIZE << " чисел: " << endl;
+        for (size_t i = 0; i < ARR_SIZE; i++)
+        {
+            cin >> arr[i];
+        }
+
+        Lib::SortArray(arr, ARR_SIZE);
+        Lib::PrintArray(arr, ARR_SIZE);
+
+        delete[] arr;
+    }
+}
+
+int main()
+{
+
+    task3();
+
+    task1();
+
+    task2();
+
+    return 0;
+}
